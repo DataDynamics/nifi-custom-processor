dfa3759c6f8f03c682fb2dd85f093fed4e45d9c3
김병곤 <fharenheit@gmail.com>
2023-10-13 22:52:23 +0900

    단위 테스트 코드 추가

--- nifi-custom-processors/src/main/java/io/datadynamics/nifi/kudu/validator/TimestampValidator.java
+++ nifi-custom-processors/src/main/java/io/datadynamics/nifi/kudu/validator/TimestampValidator.java
@@ -4,19 +4,17 @@ import org.apache.nifi.components.ValidationContext;
 import org.apache.nifi.components.ValidationResult;
 import org.apache.nifi.components.Validator;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
 
 public class TimestampValidator implements Validator {
 
     @Override
     public ValidationResult validate(String subject, String value, ValidationContext context) {
         try {
-            SimpleDateFormat formatter = new SimpleDateFormat(value);
-            formatter.format(new Date());
+            DateTimeFormatter.ofPattern(value);
             return (new ValidationResult.Builder()).valid(true).input(value).subject(subject).build();
         } catch (Exception e) {
-            return (new ValidationResult.Builder()).valid(false).input(value).explanation("SimpleDateFormat으로 변환이 가능한 패턴이어야 합니다. (예; yyyy-MM-dd HH:mm:ss.SSS)").build();
+            return (new ValidationResult.Builder()).valid(false).input(value).explanation("DateTimeFormatter 또는 SimpleDateFormat으로 변환이 가능한 패턴이어야 합니다. (예; yyyy-MM-dd HH:mm:ss.SSS)").build();
         }
     }
 
--- nifi-custom-processors/src/test/java/io/datadynamics/nifi/kudu/validator/TimestampValidatorTest.java
+++ nifi-custom-processors/src/test/java/io/datadynamics/nifi/kudu/validator/TimestampValidatorTest.java
@@ -1,5 +1,23 @@
 package io.datadynamics.nifi.kudu.validator;
 
+import org.apache.nifi.components.ValidationResult;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TimestampValidatorTest {
+
+    @Test
+    public void validate_invalid() {
+        TimestampValidator validator = new TimestampValidator();
+        ValidationResult result = validator.validate("helloworld", "yasdfyyyMMddHHmmss", null);
+        Assert.assertFalse(result.isValid());
+    }
+
+    @Test
+    public void validate_valid() {
+        TimestampValidator validator = new TimestampValidator();
+        ValidationResult result = validator.validate("helloworld", "yyyyMMddHHmmss", null);
+        Assert.assertTrue(result.isValid());
+    }
 
 }
