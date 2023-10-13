1789ad8680e09cd261ff7e10ea678e908ff90f14
김병곤 <fharenheit@gmail.com>
2023-10-13 22:37:07 +0900

    단위 테스트 코드 추가

--- nifi-custom-processors/src/main/java/io/datadynamics/nifi/kudu/converter/ObjectTimestampFieldConverter.java
+++ nifi-custom-processors/src/main/java/io/datadynamics/nifi/kudu/converter/ObjectTimestampFieldConverter.java
@@ -36,44 +36,44 @@ public class ObjectTimestampFieldConverter implements FieldConverter<Object, Tim
         if (field == null) {
             return null;
         }
+
         if (field instanceof Timestamp) {
             return (Timestamp) field;
         }
+
         if (field instanceof Date) {
             final Date date = (Date) field;
             return new Timestamp(date.getTime());
         }
+
         if (field instanceof Number) {
             final Number number = (Number) field;
             return new Timestamp(number.longValue());
         }
+
         if (field instanceof String) {
             final String string = field.toString().trim();
             if (string.isEmpty()) {
                 return null;
             }
 
-            if (!StringUtils.isEmpty(timestampPattern)) {
-                final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(timestampPattern);
-                try {
-                    // 날짜 패턴으로 데이터를 파싱하고 AddHour를 추가한다.
-                    final LocalDateTime localDateTime = LocalDateTime.parse(string, formatter);
-                    if (addHour != 0) {
-                        LocalDateTime plus = localDateTime.plus(Duration.ofHours(addHour));
-                        return Timestamp.valueOf(plus);
-                    } else {
-                        return Timestamp.valueOf(localDateTime);
-                    }
-                } catch (final DateTimeParseException e) {
-                    final String message = String.format("Convert Field Name [%s] Value [%s] to Timestamp LocalDateTime parsing failed: %s", name, field, e.getMessage());
-                    throw new IllegalTypeConversionException(message);
+            final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(timestampPattern);
+            try {
+                // 날짜 패턴으로 데이터를 파싱하고 AddHour를 추가한다.
+                final LocalDateTime localDateTime = LocalDateTime.parse(string, formatter);
+                if (addHour != 0) {
+                    LocalDateTime plus = localDateTime.plus(Duration.ofHours(addHour));
+                    return Timestamp.valueOf(plus);
+                } else {
+                    return Timestamp.valueOf(localDateTime);
                 }
-            } else {
+            } catch (final DateTimeParseException e1) {
+                // 파싱 에러가 발생하면 숫자로 다시 파싱을 시도한다.
                 try {
                     final long number = Long.parseLong(string);
                     return new Timestamp(number);
-                } catch (final NumberFormatException e) {
-                    final String message = String.format("Convert Field Name [%s] Value [%s] to Timestamp Long parsing failed: %s", name, field, e.getMessage());
+                } catch (final NumberFormatException e2) {
+                    final String message = String.format("Convert Field Name [%s] Value [%s] to Timestamp Long parsing failed: %s", name, field, e2.getMessage());
                     throw new IllegalTypeConversionException(message);
                 }
             }
--- nifi-custom-processors/src/test/java/io/datadynamics/nifi/kudu/converter/ObjectTimestampFieldConverterTest.java
+++ nifi-custom-processors/src/test/java/io/datadynamics/nifi/kudu/converter/ObjectTimestampFieldConverterTest.java
@@ -1,6 +1,7 @@
 package io.datadynamics.nifi.kudu.converter;
 
 import org.apache.commons.lang3.time.DateUtils;
+import org.apache.nifi.serialization.record.util.IllegalTypeConversionException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -34,6 +35,34 @@ public class ObjectTimestampFieldConverterTest {
         ObjectTimestampFieldConverter converter = new ObjectTimestampFieldConverter();
         Timestamp output = converter.convertField(dateString, Optional.of("yyyy-MM-dd"), "helloworld", 0, "yyyy-MM-dd HH:mm:ss.SSS");
         Assert.assertEquals(current, output.getTime());
+    }
+
+    @Test
+    public void convertField_string_long() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String dateString = "2023-01-01 11:11:11.111";
+        Date date = formatter.parse(dateString);
+        long current = date.getTime();
+
+        ObjectTimestampFieldConverter converter = new ObjectTimestampFieldConverter();
+        Timestamp output = converter.convertField(String.valueOf(current), Optional.of("yyyy-MM-dd"), "helloworld", 0, "yyyy-MM-dd HH:mm:ss.SSS");
+        Assert.assertEquals(current, output.getTime());
+    }
+
+    @Test
+    public void convertField_string_invalid_long() {
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+            String dateString = "2023-01-01 11:11:11.111";
+            Date date = formatter.parse(dateString);
+            long current = date.getTime();
+
+            ObjectTimestampFieldConverter converter = new ObjectTimestampFieldConverter();
+            converter.convertField(String.valueOf(current) + "asdfasdf", Optional.of("yyyy-MM-dd"), "helloworld", 0, "yyyy-MM-dd HH:mm:ss.SSS");
+            Assert.assertTrue(false);
+        } catch (IllegalTypeConversionException | ParseException e) {
+            Assert.assertTrue(true);
+        }
     }
 
     @Test
