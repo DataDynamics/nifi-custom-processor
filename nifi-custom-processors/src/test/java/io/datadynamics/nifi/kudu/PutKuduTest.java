b39eebd3dde734cbebf502e42bb7ceaaa7a5a32f
김병곤 <fharenheit@gmail.com>
2023-10-14 00:29:54 +0900

    단위 테스트 코드를 추가함

---
+++ nifi-custom-processors/src/test/java/io/datadynamics/nifi/kudu/PutKuduTest.java
@@ -0,0 +1,6 @@
+package io.datadynamics.nifi.kudu;
+
+// https://github.com/apache/nifi/blob/rel/nifi-1.23.2/nifi-nar-bundles/nifi-kudu-bundle/nifi-kudu-processors/src/test/java/org/apache/nifi/processors/kudu/TestPutKudu.java
+public class PutKuduTest {
+
+}
