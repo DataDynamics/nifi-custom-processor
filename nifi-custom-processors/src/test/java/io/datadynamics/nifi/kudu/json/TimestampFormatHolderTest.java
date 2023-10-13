3c078d07308eab22cbe7bddccb3466f385875dce
김병곤 <fharenheit@gmail.com>
2023-10-13 23:04:02 +0900

    단위 테스트 코드 추가

--- nifi-custom-processors/src/test/java/io/datadynamics/nifi/kudu/json/TimestampFormatHolderTest.java
+++ nifi-custom-processors/src/test/java/io/datadynamics/nifi/kudu/json/TimestampFormatHolderTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TimestampFormatHolderTest {
 
     @Test
-    public void get() {
+    public void getPattern() {
         try {
             ObjectMapper mapper = new ObjectMapper();
             String json = "{\n" +
@@ -23,6 +23,28 @@ public class TimestampFormatHolderTest {
             TimestampFormatHolder holder = new TimestampFormatHolder(timestampFormats);
 
             Assert.assertEquals("yyyy-MM-dd HH:mm:ss", holder.getPattern("COL_TIMESTAMP"));
+        } catch (Exception e) {
+            Assert.assertFalse(true);
+        }
+    }
+
+    @Test
+    public void getType() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String json = "{\n" +
+                    "  \"formats\": [\n" +
+                    "    {\n" +
+                    "      \"column-name\": \"COL_TIMESTAMP\",\n" +
+                    "      \"timestamp-pattern\": \"yyyy-MM-dd HH:mm:ss\",\n" +
+                    "      \"type\": \"TIMESTAMP_MILLIS\"\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}";
+            TimestampFormats timestampFormats = mapper.readValue(json, TimestampFormats.class);
+            TimestampFormatHolder holder = new TimestampFormatHolder(timestampFormats);
+
+            Assert.assertEquals("TIMESTAMP_MILLIS", holder.getType("COL_TIMESTAMP").toString());
         } catch (Exception e) {
             Assert.assertFalse(true);
         }
