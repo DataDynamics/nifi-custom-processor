3dee07325ec2e9a2d5a2661b0eb5e582a04af3f7
김병곤 <fharenheit@gmail.com>
2023-10-13 22:25:44 +0900

    단위 테스트 코드 추가

--- nifi-custom-processors/src/test/java/io/datadynamics/nifi/kudu/converter/ObjectTimestampFieldConverterTest.java
+++ nifi-custom-processors/src/test/java/io/datadynamics/nifi/kudu/converter/ObjectTimestampFieldConverterTest.java
@@ -1,5 +1,38 @@
 package io.datadynamics.nifi.kudu.converter;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+
 public class ObjectTimestampFieldConverterTest {
+
+    @Test
+    public void convertField_long() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String dateString = "2023-01-01 11:11:11.111";
+        Date date = formatter.parse(dateString);
+        long current = date.getTime();
+
+        ObjectTimestampFieldConverter converter = new ObjectTimestampFieldConverter();
+        Timestamp output = converter.convertField(current, Optional.of("yyyy-MM-dd"), "helloworld", 0, "yyyy-MM-dd HH:mm:ss.SSS");
+        Assert.assertEquals(current, output.getTime());
+    }
+
+    @Test
+    public void convertField_string() throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String dateString = "2023-01-01 11:11:11.111";
+        Date date = formatter.parse(dateString);
+        long current = date.getTime();
+
+        ObjectTimestampFieldConverter converter = new ObjectTimestampFieldConverter();
+        Timestamp output = converter.convertField(dateString, Optional.of("yyyy-MM-dd"), "helloworld", 0, "yyyy-MM-dd HH:mm:ss.SSS");
+        Assert.assertEquals(current, output.getTime());
+    }
 
 }
